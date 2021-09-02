d74ea0857fa968324c1c47c1b0ce12b5664d01d7
André Carmo <andre_de11@msn.com>
2021-09-02 10:59:25 -0300

    feat: adding Ticket Entity, constructor and migration file

---
+++ src/entities/Ticket.ts
@@ -0,0 +1,40 @@
+import TicketAlreadyPurchasedError from "@/errors/TicketAlreadyPurchased";
+import TicketData from "@/interfaces/ticket";
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity("ticket")
+export default class Ticket extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  isOnline: boolean;
+
+  @Column()
+  hasHotelReservation: boolean;
+
+  @Column("boolean", { default: false })
+  isPaid: boolean;
+
+  @Column()
+  userId: number;
+
+  populateFromData(data: TicketData) {
+    this.isOnline = data.isOnline;
+    this.hasHotelReservation = data.hasHotelReservation;
+    this.isPaid = data.isPaid;
+    this.userId = data.userId;
+  }
+
+  static async createOrUpdate(data: TicketData) {
+    let ticket = await this.findOne({ where: { userId: data.userId } });
+
+    if (ticket.isPaid) {
+      throw new TicketAlreadyPurchasedError();
+    }
+
+    ticket ||= Ticket.create();
+    ticket.populateFromData(data);
+    await ticket.save();
+  }
+}
